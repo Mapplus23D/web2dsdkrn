7fc2584b6357fccdc6211cb89dde6ee692dff092
yangshanglong <dragonyang0917@gmail.com>
2025-07-09 16:38:58 +0800

    新增许可失败信息

--- src/utils/LicenseUtil.ts
+++ src/utils/LicenseUtil.ts
@@ -6,9 +6,14 @@ export const active = async (code?: string) => {
     // 激活序列号，替换为有效的序列号
     const serial = code || 'VEJZU-QQY5C-7YQ2G-RQ2CS-M6RTH'
     const result = await RTNWebMap?.activate(serial)
-    if (result) {
+    if (result.success) {
       license = await RTNWebMap?.getLicenseInfo()
+    } else {
+      console.warn(result.message)
     }
+  }
+  if (!license?.isValid) {
+    console.warn(license ? license.message : '没有获取到许可')
   }
   return license
 }
